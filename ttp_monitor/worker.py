93f454d43334be48b5c6b3c7dd149c2660ecc5b4
Jason Walton <jwalton512@gmail.com>
2023-11-19 18:07:11 -0700

    fix throttling after notification sent

--- ttp_monitor/worker.py
+++ ttp_monitor/worker.py
@@ -6,43 +6,31 @@ from typing import Any, Callable
 from ttp_monitor.watcher import Watcher
 
 PROCESS_THROTTLE_SECS = 30
-PROCESS_AFTER_NOTIFY_THROTTLE_SECS = 300
+AFTER_SUCCESS_THROTTLE_SECS = 300
 
 
 class Worker:
     def __init__(self, location: int, start_date: date, end_date: date) -> None:
         self.watcher = Watcher(location, start_date, end_date)
-        self.last_result = False
 
     def run(self) -> None:
         while True:
-            self.last_result = self._throttle(
-                self._process_running, self._throttle_secs
-            )
-
-    @property
-    def _throttle_secs(self) -> int:
-        throttle = int(os.environ.get("PROCESS_THROTTLE_SECS", PROCESS_THROTTLE_SECS))
-        after_notify_throttle = int(
-            os.environ.get(
-                "PROCESS_AFTER_NOTIFY_THROTTLE_SECS", PROCESS_AFTER_NOTIFY_THROTTLE_SECS
-            )
-        )
-        return after_notify_throttle if self.last_result else throttle
-
-    def _throttle(
-        self, func: Callable[..., Any], throttle_secs: float, *args, **kwargs
-    ) -> Any:
-        start_time = time.time()
-        result = func(*args, **kwargs)
-        elapsed = time.time() - start_time
-        sleep_duration = throttle_secs - elapsed
-
-        self._sleep(sleep_duration)
-        return result
+            self._process()
 
     def _sleep(self, duration: float) -> None:
         time.sleep(duration)
 
-    def _process_running(self) -> bool:
-        return self.watcher.process()
+    def _process(self) -> None:
+        start_time = time.time()
+        result = self.watcher.process()
+        elapsed = time.time() - start_time
+
+        throttle_secs = float(
+            os.environ.get("PROCESS_THROTTLE_SECS", PROCESS_THROTTLE_SECS)
+        )
+        after_success_throttle_secs = float(
+            os.environ.get("AFTER_SUCCESS_THROTTLE_SECS", AFTER_SUCCESS_THROTTLE_SECS)
+        )
+
+        duration = (after_success_throttle_secs if result else throttle_secs) - elapsed
+        self._sleep(duration)
